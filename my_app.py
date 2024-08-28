95752b1bf16ec470daaab0103538c1b8cdf5fed3
E <158484665+EDemirok999@users.noreply.github.com>
2024-08-28 18:03:17 +0300

    my_app.py

--- my_app.py
+++ my_app.py
@@ -5,7 +5,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image  # to deal with images (PIL: Python imaging library)
-
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge, Lasso
 
 
 
